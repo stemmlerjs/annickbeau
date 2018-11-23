4851a9a95c58fd03db19093372fe3b0dbdc3039f
Khalil Stemmler <metroidman12@gmail.com>
2018-11-23 16:27:52 -0500

    Updated meta tags.

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,11 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Iconnick Makeup | Toronto Makeup Artist - Annick Beau" />
+    <Helmet 
+      title="Iconnick Makeup | Toronto Makeup Artist - Annick Beau" 
+      description="Annick Beau is a makeup artist in Toronto"
+      keywords="makeup, toronto, makeup artist, mua, annick beau, toronto makeup"
+    />
     {/* <Navbar/> */}
     <div>{children}</div>
   </div>
